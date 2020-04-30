495ed92c789aa6a3c2098127b44d620caf02cd02
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-04-30 11:39:27 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using LinqConsoleApp;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            //d.Przyklad1();
+        }
+    }
+}
